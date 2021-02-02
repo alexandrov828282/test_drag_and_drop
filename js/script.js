caa3fd7604341f8fdd82616e94c246890e943942
alexandrov828282 <alexandrov828282@gmail.com>
2021-02-02 14:53:40 +0500

    020220211453

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let candyElem = document.querySelector('.candy');
+
+candyElem.addEventListener('dragstart', ()=>{
+        console.log('захват произошёл');
+    }
+
+)
+
+
+
+console.log(candyElem);
